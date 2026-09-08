1329819660564ee8ad02f1d60675c3bc087776f5
dity <275731998+dity42@users.noreply.github.com>
2026-09-08 19:06:55 +0300

    refactor: extract parser logic into separate functions

--- src/kohai/actions/search.py
+++ src/kohai/actions/search.py
@@ -6,23 +6,39 @@ PARSER2 = AnimegoParser()
 def anisearch(atitle: str):
     try: 
         query = PARSER1.search(title=atitle, limit=None, strict=True, only_anime=False, include_material_data=False)
+        return _first_parser_results(query)
     except: 
         query = PARSER2.search(query=atitle)
+        return _second_parser_results(query)
 
+def _first_parser_results(query): 
     result = []
-
     for anime in query:
         title = anime.get('title', 'Unknown')
         year = anime.get('year', 'N/A')
         shikimori_id = anime.get('shikimori_id')
         additional = anime.get('additional_data', {})
         last_ep = additional.get('last_episode', 0)
-
         info = {
             'title': title,
             'year': year,
             'last_episode': last_ep,
-            'shikimori_id': shikimori_id, 
+            'shikimori_id': shikimori_id,
+        }
+        result.append(info)
+    return result
+
+def _second_parser_results(query):
+    result = []
+    for anime in query:
+        title = anime.get('title', 'Unknown')
+        year = anime.get('year', 'N/A')
+        id = anime.get('id')
+        info = {
+            'title': title,
+            'year': year,
+            'last_episode': None,
+            'shikimori_id': None,
         }
         result.append(info)
     return result
