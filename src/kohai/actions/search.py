4ddc73bc27fcd1323b3db01a53362ce95e484f88
dity <275731998+dity42@users.noreply.github.com>
2026-09-09 19:00:59 +0300

    refactor: use list comprehension for parser results and move parsers in other file

--- src/kohai/actions/search.py
+++ src/kohai/actions/search.py
@@ -1,43 +1,13 @@
 from anime_parsers_ru import KodikParser, AnimegoParser
 
-PARSER1 = KodikParser()
-PARSER2 = AnimegoParser()
+from kohai.utils.parse_results import _first_parser_results, _second_parser_results
 
 def anisearch(atitle: str):
     try: 
-        query = PARSER1.search(title=atitle, limit=None, strict=True, only_anime=False, include_material_data=False)
+        parser = KodikParser()
+        query = parser.search(title=atitle, limit=None, strict=True, only_anime=False, include_material_data=False)
         return _first_parser_results(query)
     except: 
-        query = PARSER2.search(query=atitle)
+        parser = AnimegoParser()
+        query = parser.search(query=atitle)
         return _second_parser_results(query)
-
-def _first_parser_results(query): 
-    result = []
-    for anime in query:
-        title = anime.get('title', 'Unknown')
-        year = anime.get('year', 'N/A')
-        shikimori_id = anime.get('shikimori_id')
-        link = anime.get('link')
-        info = {
-            'title': title,
-            'year': year,
-            'shikimori_id': shikimori_id,
-            'link': link,
-        }
-        result.append(info)
-    return result
-
-def _second_parser_results(query):
-    result = []
-    for anime in query:
-        title = anime.get('title', 'Unknown')
-        year = anime.get('year', 'N/A')
-        id = anime.get('id')
-        info = {
-            'title': title,
-            'year': year,
-            'last_episode': None,
-            'shikimori_id': None,
-        }
-        result.append(info)
-    return result
---
+++ src/kohai/utils/parse_results.py
@@ -0,0 +1,22 @@
+def _first_parser_results(query): 
+    return [
+        {
+            'title': anime.get('title', 'Unknown'),
+            'year': anime.get('year', 'N/A'),
+            'shikimori_id': anime.get('shikimori_id'),
+            'link': anime.get('link'),
+        }
+        for anime in query
+    ]
+
+def _second_parser_results(query):
+    return [
+        {
+            'title': anime.get('title', 'Unknown'),
+            'or_title': anime.get('original_title', 'Unknown'),
+            'year': anime.get('year', 'N/A'),
+            'link': anime.get('link'),
+            'id': anime.get('id'),
+        }
+        for anime in query
+    ]
